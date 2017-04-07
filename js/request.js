85a5b9e9187df1b3aaaa77c0ee5890bf518978c3
Algys Ievlev <ivalgys1997@gmail.com>
2017-04-07 21:37:11 +0300

    all merged

---
+++ js/request.js
@@ -0,0 +1,86 @@
+/**
+ * Created by algys on 10.03.17.
+ */
+/*
+ Example:
+
+ myHeaders = new Headers({
+ "Content-Type": "text/plain",
+ "Content-Length": content.length.toString(),
+ "X-Custom-Header": "ProcessThisImmediately",
+ });
+
+ fetch(myRequest)
+ .then(function(response) {
+ return response.blob();
+ });
+
+ .then(function(myBlob) {
+ var objectURL = URL.createObjectURL(myBlob);
+ myImage.src = objectURL;
+ });
+
+ */
+
+'use strict';
+
+const ALLOWED_METHODS = ['POST', 'PUT', 'PATCH', 'DELETE'];
+
+class Request {
+    constructor(server) {
+        this.server = server;
+
+        this.funcs = [function(response) {
+            if(response.status >= 200 && response.status < 300) {
+                return response.json();
+            } else {
+                throw new Error();
+            }
+        }];
+
+        this.baseCatch = function (error) {
+            console.log('[FAIL] ', error);
+        };
+
+        this.json = '{}';
+    }
+
+    addResponse(_func) {
+        this.funcs.push(_func);
+        return this;
+    }
+
+    addJson(_params)  {
+        this.json = JSON.stringify(_params);
+        return this;
+    }
+
+    error(_errorCallback) {
+        this.baseCatch = _errorCallback;
+        return this;
+    }
+
+    request(path, data) {
+        data = data || {};
+
+        if(!(data['method'] && (data['method'] in ALLOWED_METHODS)))
+            data['method'] = data['method'] || 'GET';
+
+        data['headers'] = data['headers'] || {"Content-Type": "application/json"};
+        data['mode'] = data['mode'] || 'CORS';
+        data['cache'] = data['cache'] || 'default';
+
+        data['body'] = this.json;
+
+        let fetchPromise = fetch(this.server + path, data);
+
+        let me = this;
+        this.funcs.map(function(el, index) {
+            fetchPromise.then(el);
+        });
+
+        fetchPromise.catch(me.baseCatch); // TODO catch
+    }
+}
+
+export default Request;
---
+++ js/router.js
@@ -0,0 +1,90 @@
+class Router {
+    constructor(node) {
+        this.node = node;
+        this.routes = {};
+    }
+
+    /**
+     * Регистрация маршрута
+     * @param {string} route
+     * @param {BaseView} view
+     */
+    register(route, view) {
+        this.routes[route] = view;
+    }
+
+    _getViewByRoute(href) {
+        return this.routes[href];
+    }
+
+
+    onRouteChange(event) {
+
+        if (!event.target instanceof HTMLAnchorElement) {
+            return;
+        }
+
+        if (this.go(event.target.getAttribute('href'))) {
+            event.preventDefault();
+        }
+
+    }
+
+    start() {
+
+        this.node
+            .addEventListener('click', event => this.onRouteChange(event));
+
+
+        this.currentView = this._getViewByRoute(location.pathname);
+    }
+
+    /**
+     * Перетий по маршруту
+     * @param {string} path
+     * @return {boolean} - если есть маршрурт
+     */
+    go(path) {
+        if(path == '/back'){
+            window.history.back();
+            let back = location.pathname;
+            this.go(back);
+            return true;
+        }
+
+        let view = this._getViewByRoute(path);
+
+        if (!view) {
+            return false;
+        }
+
+        if (this.currentView === view) {
+            return true;
+        }
+
+        view.show();
+        let obj = { page: 1 };
+        window.history.pushState(obj, '', path);
+
+        if(this.currentView) {
+            this.currentView.hide();
+        }
+
+        this.currentView = view;
+        return true;
+    }
+
+    startPage(url){
+        for(let key in this.routes){
+            let view = this._getViewByRoute(key);
+            view.hide();
+        }
+        let view = this._getViewByRoute(url);
+        view.show();
+        let obj = { page: 1 };
+        window.history.pushState(obj, '', url);
+    }
+
+}
+
+export default Router;
